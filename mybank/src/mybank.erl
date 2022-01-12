f9c68bca1b95c9feed182fae476b5d6a95467dc5
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-01-11 20:56:58 -0500

    :tada: Refined version of mybank module

--- mybank/src/mybank.erl
+++ mybank/src/mybank.erl
@@ -1,17 +1,84 @@
 -module(mybank).
 
--export([start/0, stop/1]).
--export([main_loop/0]).
+-export([start/0, stop/0, deposit/2, balance/1, withdraw/2]).
+-export([init/0]).
 
+-record(state, {
+    accounts
+}).
+
+%%========= API =========%%
 start() ->
     io:format("~n~n Opening the bank...~n~n"),
-    spawn(?MODULE, main_loop, []).
+    Pid = spawn(?MODULE, init, []),
+    register(?MODULE, Pid).
 
-stop(Pid) ->
-    Pid ! terminate.
+stop() ->
+    ?MODULE ! terminate.
 
-main_loop() ->
+deposit(AccountId, Amount) ->
+    ?MODULE ! {deposit, self(), AccountId, Amount},
     receive
+        Reply -> Reply
+    after 5000 ->
+        {error, timeout}
+    end.
+
+withdraw(AccountId, Amount) ->
+    ?MODULE ! {withdraw, self(), AccountId, Amount},
+    receive
+        Reply -> Reply
+    after 5000 ->
+        {error, timeout}
+    end.
+
+balance(AccountId) ->
+    ?MODULE ! {balance, self(), AccountId},
+    receive
+        Reply -> Reply
+    after 5000 ->
+        {error, timeout}
+    end.
+
+%%========= Internal =========%%
+init() ->
+    Accounts = dict:new(),
+    State = #state{accounts = Accounts},
+    main_loop(State).
+
+main_loop(#state{
+    accounts = Accounts
+} = State) ->
+    receive
+        {deposit, CallerPid, AccountId, Amount} ->
+            CurrentBalance = get_current_balance(AccountId, Accounts),
+            AccountsNew = dict:store(AccountId, CurrentBalance + Amount, Accounts),
+            io:format("~n~n ~p deposited ~p ~n", [AccountId, Amount]),
+            CallerPid ! ok,
+            main_loop(State#state{accounts = AccountsNew});
+        {balance, CallerPid, AccountId} ->
+            CallerPid ! {ok, get_current_balance(AccountId, Accounts)},
+            main_loop(State);
+        {withdraw, CallerPid, AccountId, Amount} ->
+            case get_current_balance(AccountId, Accounts) < Amount of
+                true ->
+                    io:format("~n~n ~p cannot withdraw ~p ~n", [AccountId, Amount]),
+                    CallerPid ! {error, insufficient_funds},
+                    main_loop(State);
+                false ->
+                    CurrentBalance = get_current_balance(AccountId, Accounts),
+                    AccountsNew = dict:store(AccountId, CurrentBalance - Amount, Accounts),
+                    io:format("~n~n ~p withdrew ~p ~n", [AccountId, Amount]),
+                    CallerPid ! ok,
+                    main_loop(State#state{accounts = AccountsNew})
+            end;
         terminate ->
             io:format("~n~n Closing the bank...~n~n")
+    end.
+
+
+get_current_balance(AccountId, Accounts) ->
+    case dict:find(AccountId, Accounts) of
+        error -> 0;
+        {ok, Amount0} -> Amount0
     end.
